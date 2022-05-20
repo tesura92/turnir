5776c59b6d0af37d58ff1ba8c5328cb464c29785
tesura92 <tesura92@gmail.com>
2022-05-20 03:41:26 +0200

    int
    int

--- main.py
+++ main.py
@@ -9,10 +9,29 @@ import math
 class Tournament:
 
     def __init__(self, players, typeoft):
-        self.player_list = players
+        self.player_list = self.shuffleplayers(players)
         self.__tournament__ = typeoft
 
-    def window(self):
+    def set_playerlist(self, new_list):
+        self.player_list = new_list
+
+    def shuffleplayers(self, players):
+        random.shuffle(players)
+        random.shuffle(players)
+        random.shuffle(players)
+        return players
+
+
+class SingleElimination(Tournament):
+    def __init__(self, players, typeoft):
+        Tournament.__init__(self, players, typeoft)
+
+    def start(self):
+
+        if len(self.player_list) == 1:
+            popwinner(self.player_list)
+            return
+
         window = Tk()
         window.geometry('300x600')
 
@@ -25,26 +44,160 @@ class Tournament:
         lista1 = self.player_list[:len(self.player_list) // 2]
         lista2 = self.player_list[len(self.player_list) // 2:]
         lista2.reverse()
+        boxes = []
         for i in range((len(self.player_list)) // 2):
             verus = [lista1[i], lista2[i]]
             tlabel = Label(window, text=f'{lista1[i]} VS {lista2[i]}')
             tlabel.grid(row=i, column=0, padx=2, pady=2)
             t = ttk.Combobox(window, values=verus)
             t.grid(row=i, column=1, padx=2, pady=2)
-        submitresults = Button(window, text='Submit Results', command=choice_sel)
+            boxes.append(t)
+        submitresults = Button(window, text='Submit Results',
+                               command=lambda: choice_sel(self, boxes, window, submitresults))
         submitresults.grid(row=((len(self.player_list)) // 2) + 1, column=0)
 
         window.mainloop()
 
+    def nextround(self, window):
+        window.destroy()
+        self.start()
 
-def choice_sel(*args, **kwargs):
-    listapobeda = [args[0].widget.get()]
-    print(listapobeda)
+
+class DoubleElimination(Tournament):
+    def __init__(self, players, typeoft):
+        Tournament.__init__(self, players, typeoft)
+
+    def start(self):
+        if len(self.player_list) == 1:
+            popwinner(self.player_list)
+            return
+
+        """
+        listapoeba = SingleElimination(ucesnici)
+        gubitnici = ucesnici
+        gubitnici.pop(gubitnici.index(listapoeba[0]))
+        listagubitnika = SingleElimination(gubitnici)
+        print(f'Unesite broj poena {listapoeba[0]} vs {listagubitnika[0]}')
+        a = int(input(f'Uneti broj poena za {listapoeba[0]} : '))
+        b = int(input(f'Uneti broj poena za {listagubitnika[0]} : '))
+        if a >= b:
+            return listapoeba[0]
+        else:
+            return listagubitnika[0]
+        """
+
+        window = Tk()
+        window.geometry('300x600')
+
+        window.mainloop()
+
+
+class Leaderboard(Tournament):
+    def __init__(self, players, typeoft):
+        Tournament.__init__(self, players, typeoft)
+
+    def start(self):
+        if len(self.player_list) == 1:
+            popwinner(self.player_list)
+            return
+
+        """
+        print()
+        rezultat = []
+        for i in range(len(ucesnici)):
+            a = int(input(f'Unesti rank {ucesnici[int(i)]}'))
+            rezultat.append(a)
+        a = rezultat.index(min(rezultat))
+        print(f'Pobednik je {ucesnici[a]}')
+        """
+
+
+        window = Tk()
+        window.geometry('300x600')
+
+        window.mainloop()
+
+
+class Swiss(Tournament):
+    def __init__(self, players, typeoft):
+        Tournament.__init__(self, players, typeoft)
+
+    def start(self):
+        if len(self.player_list) == 1:
+            popwinner(self.player_list)
+            return
+
+        """
+        a = [2 ** i for i in range(10) if len(ucesnici) <= 2 ** i]
+        while len(ucesnici) < a[0]:
+            ucesnici.append('bye')
+        b = int(math.log(a, 2))
+        drzac = [[] * b] * (b + 1)
+        drzac[0] = ucesnici
+        for i in range(1, b + 1):  # ovde prolazimo kroz runde
+            for y in range(0, len(drzac[0]) + 1, 2):
+                print(f'Unesite broj poena {drzac[i - 1][y]} vs {drzac[i - 1][y + 1]}')
+                a = int(input(f'Uneti broj poena za {drzac[i - 1][y]} : '))
+                b = int(input(f'Uneti broj poena za {drzac[i - 1][y + 1]}: '))
+                if a >= b:
+                    return
+                else:
+                    return
+        """
+
+
+
+    window = Tk()
+        window.geometry('300x600')
+
+        window.mainloop()
+
+
+class FreeForAll(Tournament):
+    def __init__(self, players, typeoft):
+        Tournament.__init__(self, players, typeoft)
+
+    def start(self):
+        if len(self.player_list) == 1:
+            popwinner(self.player_list)
+            return
+
+        """
+        drzac = [0] * len(ucesnici)
+        for i in range(len(ucesnici)):
+            for y in range(i + 1, len(ucesnici)):
+                print(f'Unesite broj poena {ucesnici[i]} vs {ucesnici[y]}')
+                drzac[i] = drzac[i] + int(input(f'{ucesnici[i]} : '))
+                drzac[y] = drzac[y] + int(input(f'{ucesnici[y]} : '))
+        print(drzac)
+        a = drzac.index(max(drzac))
+        print(f'Pobednik je {ucesnici[a]} sa {drzac[a]} bodova')
+        """
+
+        window = Tk()
+        window.geometry('300x600')
+
+        window.mainloop()
+
+
+def choice_sel(tournament, boxes, window, submitresults):
+    i = (len(tournament.player_list) // 2) + 1
+    lista = []
+    for box in boxes:
+        if box.get() == '':
+            return
+        lista.append(box.get())
+    tournament.set_playerlist(lista)
+    submitresults.destroy()
+    nextround = Button(window, text='Next Round', command=lambda: tournament.nextround(window))
+    nextround.grid(row=i, column=1)
+    print(tournament.player_list)
 
 
 def popwinner(player_list):
+    root.destroy()
     window = Tk()
-    window.title('Error massage!')
+    window.title('Winner!!!')
     window.geometry('400x100')
     msg = Label(window, text=f'Pobednik je : {player_list[0]}')
     msg.pack()
@@ -67,14 +220,32 @@ def popmsg():
 def turnir(a, window2):
     if len(listofplayer) < 1:
         popmsg()
+    elif len(listofplayer) == 1:
+        window2.destroy()
+        popwinner(listofplayer)
+        root.destroy()
     typeoft = a.get()
     window2.destroy()
-    random.shuffle(listofplayer)
-    random.shuffle(listofplayer)
-    random.shuffle(listofplayer)
-    print(listofplayer)
-    test = Tournament(listofplayer, typeoft)
-    test.window()
+    tournament = None
+    if typeoft == 'SingleElimination':
+        tournament = SingleElimination(listofplayer, typeoft)
+
+    elif typeoft == 'DoubleElimination':
+        tournament = SingleElimination(listofplayer, typeoft)
+
+    elif typeoft == 'Leaderboard':
+        tournament = SingleElimination(listofplayer, typeoft)
+
+    elif typeoft == 'Swiss':
+        tournament = SingleElimination(listofplayer, typeoft)
+
+    elif typeoft == 'FreeForAll':
+        tournament = SingleElimination(listofplayer, typeoft)
+    if tournament:
+        tournament.start()
+    else:
+        root.destroy()
+        return
 
 
 def player(a, window2):
